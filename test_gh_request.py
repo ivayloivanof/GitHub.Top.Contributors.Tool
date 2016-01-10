50baca7a3c17b472adba9957c6a874277a1eedc4
ivayloivanof <ivaylo.ivanof@gmail.com>
2016-01-10 12:52:48 +0200

    rename:  xrange to range; replace '' with '

--- test_gh_request.py
+++ test_gh_request.py
@@ -1,15 +1,15 @@
 from gh_request import *
 
-astropy_contributors = ['eteq', "mdboom", "astrofrog", "embray", "taldcroft",
-	"mhvk", "mdmueller", "adrn", "keflavich", "larrybradley"]
+astropy_contributors = ['eteq', 'mdboom', 'astrofrog', 'embray', 'taldcroft',
+	'mhvk', 'mdmueller', 'adrn', 'keflavich', 'larrybradley']
 
 def test_get_top_ten_contributors():
-    print("Testing get_top_ten_contributors")
-    contributors_json = get_contributors("astropy", "astropy")
+    print('Testing get_top_contributors')
+    contributors_json = get_contributors('astropy', 'astropy')
     contributors = get_contributor_details(contributors_json)
-    for i in xrange(10):
+    for i in range(10):
         (name, count) = contributors[i]
         assert(name == astropy_contributors[i])
-    print("Passed test")
+    print('Passed test')
 
 test_get_top_ten_contributors()
